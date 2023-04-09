b98b2ce44591149a8531498eac0c6882b21035cb
Emircan Tamer <e.tamer_@hotmail.com>
2023-04-10 01:08:41 +0300

    level design2

--- OUA Game Jam Project/Assets/Scenes/OUR SCENES/Irem/scripts/character/CharacterSelections.cs
+++ OUA Game Jam Project/Assets/Scenes/OUR SCENES/Irem/scripts/character/CharacterSelections.cs
@@ -5,9 +5,19 @@ namespace Character
 {
     public class CharacterSelections : MonoBehaviour
     {
+
+        PlayerMovement playerMovement;
+        [SerializeField] GameObject cart;
+
         // tags for unity level selection objects (update and fixedUpdate)
         private const string _update = "Update", _fixedUpdate = "FixedUpdate";
         private const string _groceryCart = "GroceryCart", _suprise = "Suprise";
+
+        private void Start()
+        {
+            playerMovement = GetComponent<PlayerMovement>();
+        }
+
 
         private void OnTriggerEnter(Collider other)
         {
@@ -15,18 +25,20 @@ namespace Character
             {
                 if (other.CompareTag(_update))
                 {
-                    Debug.Log("Update");
+                    playerMovement.speed += 2;
                 }
                 else if (other.CompareTag(_fixedUpdate))
                 {
-                    Debug.Log("FixedUpdate");
+                    playerMovement.speed += 4;
+
                 }
             }
             else if (other.GetComponent<GitLevelSelections>())
             {
                 if (other.CompareTag(_groceryCart))
                 {
-                    Debug.Log("GroceryCart");
+                    cart.SetActive(true);
+                    other.gameObject.SetActive(false);
                 }
                 else if (other.CompareTag(_suprise))        // TO-DO: plane, skateboard, donkey etc.
                 {
